88ece9f8f3170cb22ece801fe1b82d1b9025fef0
vrie <vrie@vries-MacBook-Air.local>
2023-03-19 19:17:09 +0800

    Buku Pretty

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -29,15 +29,17 @@
                             </div>
                             <div class="form-group">
                                 <label for="isbn">ISBN</label>
-                                <input type="number" class="form-control" value="{{ old('isbn') }}" id="isbn" name="isbn" placeholder="Isbn" autocomplete="off">
+                                <input type="number" class="form-control" value="{{ old('isbn') }}" id="isbn"
+                                    name="isbn" placeholder="Isbn" autocomplete="off">
                             </div>
                             <div class="form-group">
                                 <label for="judul">Judul</label>
-                                <input type="text" class="form-control" value="{{ old('judul') }}" id="judul" name="judul" placeholder="Judul" autocomplete="off">
+                                <input type="text" class="form-control" value="{{ old('judul') }}" id="judul"
+                                    name="judul" placeholder="Judul" autocomplete="off">
                             </div>
                             <div class="form-group">
                                 <label>Kategori</label>
-                                 <x-adminlte-select2 class="form-control" style="width: 100%;" name="kategori">
+                                <x-adminlte-select2 class="form-control" style="width: 100%;" name="kategori">
                                     <optgroup label="Pilih Kategori">
                                         @foreach($kategori as $kat)
                                         <option value="{{ $kat->id }}">{{ $kat->nama }}</option>
@@ -46,7 +48,7 @@
                                 </x-adminlte-select2>
                                 <div class="form-group">
                                     <label>Rak</label>
-                                     <x-adminlte-select2 class="form-control" style="width: 100%;" name="rak">
+                                    <x-adminlte-select2 class="form-control" style="width: 100%;" name="rak">
                                         <optgroup label="Pilih Rak">
                                             @foreach($rak as $itm)
                                             <option value="{{ $itm->id }}">{{ $itm->nama }}</option>
@@ -76,11 +78,14 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="tahun">Tahun</label>
-                                    <input type="number" class="form-control" value="{{ old('tahun') }}" id="tahun" name="tahun" placeholder="Tahun" autocomplete="off">
+                                    <input type="number" class="form-control" value="{{ old('tahun') }}" id="tahun"
+                                        name="tahun" placeholder="Tahun" autocomplete="off">
                                 </div>
                                 <div class="form-group">
                                     <label for="jumlah_buku">Jumlah Buku</label>
-                                    <input type="number" class="form-control" value="{{ old('jumlah_buku') }}" id="jumlah_buku" name="jumlah_buku" placeholder="Jumlah buku" autocomplete="off">
+                                    <input type="number" class="form-control" value="{{ old('jumlah_buku') }}"
+                                        id="jumlah_buku" name="jumlah_buku" placeholder="Jumlah buku"
+                                        autocomplete="off">
                                 </div>
                                 <div class="form-group">
                                     <label for="lampiran_buku">Lampiran Buku</label>
@@ -89,16 +94,18 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="keterangan_lain">keterangan Lain</label>
-                                    <textarea input type="textarea" class="form-control" value="{{ old('keterangan_lain') }}" id="keterangan_lain" name="keterangan_lain" placeholder="keterangan lain" autocomplete="off"></textarea>
+                                    <textarea input type="textarea" class="form-control"
+                                        value="{{ old('keterangan_lain') }}" id="keterangan_lain" name="keterangan_lain"
+                                        placeholder="keterangan lain" autocomplete="off"></textarea>
                                 </div>
                             </div>
                         </div>
+                        <div class="card-footer">
+                        <button type="submit" class="btn btn-info float-right">
+                            Simpan</button>
+                        </div>
                 </div>
             </div>
-        </div>
-        <div class="card-body">
-            <button type="submit" class="btn btn-info float-right">
-                Simpan</button>
         </div>
     </div>
 </div>
@@ -108,44 +115,44 @@
 
 @section('js')
 <script>
-    $('.pas-delete-metu-alert-cantik').click(function(event) {
-        var form = $(this).closest("form");
-        var name = $(this).data("name");
-        event.preventDefault();
-        Swal.fire({
-            title: "PERHATIAN",
-            text: "Setelah di hapus, anda tidak akan dapat memulihkan data ini!",
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yakin!'
-        }).then((diHapus) => {
-            if (diHapus.value) {
-                form.submit();
-            }
-        });
-        return false;
-    });
-
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-            toast.addEventListener('mouseenter', Swal.stopTimer)
-            toast.addEventListener('mouseleave', Swal.resumeTimer)
+$('.pas-delete-metu-alert-cantik').click(function(event) {
+    var form = $(this).closest("form");
+    var name = $(this).data("name");
+    event.preventDefault();
+    Swal.fire({
+        title: "PERHATIAN",
+        text: "Setelah di hapus, anda tidak akan dapat memulihkan data ini!",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yakin!'
+    }).then((diHapus) => {
+        if (diHapus.value) {
+            form.submit();
         }
-    })
+    });
+    return false;
+});
 
-    @if($message = Session::get('success'))
-    Toast.fire('Sukses !!!', '{{ $message }}', 'success')
-    @endif
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
 
-    @if($errors->any())
-    Toast.fire('Eror !!!', '{{ $errors->first() }}', 'error')
-    @endif
+@if($message = Session::get('success'))
+Toast.fire('Sukses !!!', '{{ $message }}', 'success')
+@endif
+
+@if($errors -> any())
+Toast.fire('Eror !!!', '{{ $errors->first() }}', 'error')
+@endif
 </script>
 @stop
